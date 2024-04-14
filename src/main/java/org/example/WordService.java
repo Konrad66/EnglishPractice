bff405b4aa3afb6441b33f1977cd0e3a3377f6dc
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2024-04-14 23:28:04 +0200

    read csv and print  all words

--- src/main/java/org/example/WordService.java
+++ src/main/java/org/example/WordService.java
@@ -1,13 +1,31 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class WordService {
+
+    private static final String FILE_PATH = "words.csv";
 
     List<Word> getAllWords(){
 
         List<Word> words = new ArrayList<>();
+        try(Scanner scanner = new Scanner(new File(FILE_PATH))){
+            while (scanner.hasNextLine()){
+                String text = scanner.nextLine();
+                String[] data = text.split(";");
+                String polishWord = data[0];
+                String englishWord = data[1];
+                Word word = new Word(polishWord, englishWord);
+                words.add(word);
+            }
+            System.out.println("Słowa zostały zczytane prawidłowo.");
+        } catch (FileNotFoundException e){
+            System.out.println("Nie znaleziono pliku: " + e);
+        }
         Word potato = new Word("ziemniak", "potato");
         Word tomato = new Word("pomidor", "tomato");
         words.add(tomato);
