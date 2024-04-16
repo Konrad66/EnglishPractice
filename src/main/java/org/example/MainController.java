af447faed86cd670dc50396f5549e7b2a1694714
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2024-04-16 23:56:49 +0200

    one word practice

--- src/main/java/org/example/MainController.java
+++ src/main/java/org/example/MainController.java
@@ -21,8 +21,7 @@ public class MainController {
         System.out.println("Witaj w programie do nauki słówek!");
         System.out.println("Co chcesz zrobić?");
         System.out.println("1. Wyświetl wszystkie fiszki");
-        System.out.println("2. Poćwiczmy pisanie polskiej wersji");
-        System.out.println("3. Poćwiczmy pisanie angielskiej wersji");
+        System.out.println("2. Poćwicz słówka");
         System.out.println("0. Koniec");
     }
 
@@ -39,22 +38,48 @@ public class MainController {
                 }
                 break;
             case 2:
-                practice(Language.POLISH);
-               break;
-            case 3:
-                practice(Language.ENGLISH);
+                practice();
                break;
             default:
                 System.out.println("Zły wybór. Wybierz opcje z listy.");
         }
     }
 
-    private void practice(Language typingLanguage) {
-        Language secondLanguage = typingLanguage.getSecond();
+    private void practice() {
+      oneWordPractice();
+     /*   if (wordService.isDailySesionOver()) {
+            System.out.println("Gratulacje! Wszystkie słowa przećwiczone, przyjdź jutro ćwiczyć kolejne");
+        }*/
+    }
+
+    private void oneWordPractice() {
+      /* docelowa wersja
+       PracticeSession practiceSession= wordService.getPracticeSession();
+        System.out.println("Jesteś w trybie pisania po: " + practiceSession.getLaguage());
+        System.out.println("Zostało słówek: " + practiceSession.getWordsCount());
+        Language secondLanguage = practiceSession.getLanguage().getSecond();
         Word word = wordService.getRandomWord();
         System.out.println("Przetłumacz: " + word.getWordByLanguage(secondLanguage) + "?");
         String answer = input.readText();
-        boolean correct = wordService.tryAnswer(answer, word, typingLanguage);
+        boolean correct = wordService.tryAnswer(answer, word);
+        if (correct) {
+            System.out.println("Dobra odpowiedź!");
+        } else {
+            System.out.println("Zła odpowiedź!");
+        }*/
+
+        if (wordService.getWordsCount() == 0) {
+            System.out.println("Przyjdź później, koniec słów");
+            return;
+        }
+
+        System.out.println("Jesteś w trybie pisania po: " + Language.ENGLISH);
+        System.out.println("Zostało słówek: " + wordService.getWordsCount()); // jeśli ostatnio dało 3 i odpowiedziałem dobrze to teraz da 2
+        Language secondLanguage = Language.POLISH;
+        Word word = wordService.getRandomWord(); //oczekuję że nie zwraca mi tych na które już odpowiedziałem dobrze
+        System.out.println("Przetłumacz: " + word.getWordByLanguage(secondLanguage) + "?");
+        String answer = input.readText();
+        boolean correct = wordService.tryAnswer(answer, word, secondLanguage);
         if (correct) {
             System.out.println("Dobra odpowiedź!");
         } else {
